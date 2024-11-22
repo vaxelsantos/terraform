7b732153721cf3110cdb5ee533545d51497f85f4
vaxelsantos <vanderlyaxl@gmail.com>
2024-11-22 06:51:22 -0300

    updated project

--- secrets/secrets.tf
+++ secrets/secrets.tf
@@ -1,3 +1,16 @@
-resource "aws_secretsmanager_secret" "credential-database" {
-  name = "credential"
+variable "database-rds" {
+  default = {
+    postgres = "rpmy8$QDY"
+  }
+  type = map(string)
+}
+
+resource "aws_secretsmanager_secret" "database-rds" {
+  name        = "databae-rds"
+  description = "RDS database credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "database-rds" {
+  secret_id     = aws_secretsmanager_secret.database-rds.id
+  secret_string = jsonencode(var.database-rds)
 }
